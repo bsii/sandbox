e138ededb879594ec492f0afcf8a652938e68215
Bailey Stewart <bstewart@gmail.com>
2014-11-24 21:00:11 -0800

    updates to safari experiments in javascript for automation, towards implementing a tab extraction/etc script to use with alfred

--- javascript-automation-experiments/list-safari-tabs.js
+++ javascript-automation-experiments/list-safari-tabs.js
@@ -1,24 +1,99 @@
 #!/usr/bin/env osascript -l JavaScript
 
 
-var safari = Application('Safari');
-
-if ( safari ) {
+function run(argv) {
+    console.log(JSON.stringify(argv));
     
-	windows = safari.windows();
-	var matchingUrls;
+    if ( argv[0] == undefined ) {
+        console.log("No arguments!")
+        return;
+    }
+    
+    var term = argv[0]
+    
+    var safari = Application('Safari');
+   
+    var newWindow = undefined;
+    
+    for ( window in safari.windows ) {
+        console.log("window " + window)
+        tabs = safari.windows[window].tabs.whose({ url: { _contains: term } })
+        if ( ! newWindow && tabs.count > 0) {
+//            safari.make({ "new": "document"} )
+//            newWindow = safari.windows[0]
+        }
+        for ( tab in tabs ) {
+            console.log( "tab n is " + tab + " and url is " + tabs[tab].url() );
+//            tabs[tab].move( { "to": newWindow } );
+        }
+    }
+    
+    
+//    var newWindow = safari.windows[0]
+    
+    // pretend this collects a set of tabs matching a URL pattern and moves them to a new window
+    
+    
+    // then you have to close the first tab in the new window cuz making a new window made an extra tab
 
-	var s = "test string";
-	if ( s.match(/test/) ) {
-		matchingUrls = "yes";
-	}
+    if ( newWindow ) {
+        newWindow.tabs[0].close()
+    }
+    
+    
+    
+//    console.log( safari.windows[0].tabs[0].url() )
+//    console.log( safari.windows[1].tabs[0].url() )
+//    console.log( safari.windows[1].tabs[1].url() )
+//    console.log( safari.windows[1].tabs[-1].url() )
+    
 
+//    safari.windows[1].tabs[0].move( { "to": safari.windows[0].tabs[-1] } )
+    
+//    var matchingUrls;
+//    if ( safari ) {
+//        for ( windowID in safari.windows() ) {
+//            console.log(windowID);
+//            if ( windowID === null ) {
+//                console.log("NULL")
+//            }
+//            
+//            var thisWindow = safari.windows().at(windowID);
+//            if ( thisWindow ) {
+//                for ( tabID in thisWindow.tabs() ) {
+////                    console.log(tabID)
+//                    //                var thisTab = thisWindow.tabs[tabID];
+//                    //                if ( thisTab ) {
+//                    //                    console.log(thisTab.url() );
+//                    ////                    matchingUrls.push( { "windowID": windowID, "tabID": tabID, "url": thisTab.url() } )
+//                    //                }
+//                    
+//                }
+//            }
+//        }
+//    }
+    
+//    console.log(JSON.stringify( matchingUrls ));
 
-	// for ( winId in windows ) {
-	//    thisWin = windows[winId];
-	//    for ( tabId in thisWin.tabs() ) { 
-	//		thisTab = thisWin.tabs().at(tabId);		   
-
-	//    }
-	//}
+    
 }
+
+
+    
+//    for ( window in safari.windows() ) {
+//        for ( tabId in window.tabs() ) {
+//            window.tab[tabId];
+//        }
+    
+       //	    for ( tabId in thisWin.tabs() ) {
+//			thisTab = thisWin.tabs().at(tabId);		   
+//
+//	    }
+//	}
+    
+    
+    
+//	var s = "test string";
+//	if ( s.match(/test/) ) {
+//		matchingUrls = "yes";
+//	}
